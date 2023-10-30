76dcce45a8a765704e6f701872bc0af4d607c536
Brian <brian@briankiernan.com>
2023-10-30 19:12:17 -0400

    ngrx standalone example

---
+++ ngrx-standalone/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component, inject, signal } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import {
+  ComponentStore,
+  INITIAL_STATE_TOKEN,
+  provideComponentStore,
+} from '@ngrx/component-store';
+import { TestPipe } from './test.pipe';
+
+@Component({
+  selector: 'ngrx-root',
+  standalone: true,
+  imports: [RouterModule, TestPipe],
+  template: `
+    <h1>Welcome {{ val() }}</h1>
+
+    <a routerLink="/feature">Load Feature</a>
+
+    {{ 3 | test }}
+
+    <router-outlet></router-outlet>
+  `,
+  providers: [
+    provideComponentStore(ComponentStore),
+    { provide: INITIAL_STATE_TOKEN, useValue: { test: true } },
+  ],
+})
+export class AppComponent {
+  cs = inject(ComponentStore<{ test: number }>);
+  num = signal(1);
+  val = this.cs.selectSignal((s) => s.test);
+
+  ngOnInit() {
+    this.num.set(2);
+  }
+}
