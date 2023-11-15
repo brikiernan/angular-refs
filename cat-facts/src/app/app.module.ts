6ef845fbf763f87aa4e21f77365690a3762b1639
Brian <brian@briankiernan.com>
2023-11-14 21:48:58 -0500

    add dev tools

--- cat-facts/src/app/app.module.ts
+++ cat-facts/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { EntityDataModule } from '@ngrx/data';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,6 +17,7 @@ import { DefaultDataServiceProvider, entityConfig } from './state/app-config';
     AppRoutingModule,
     HttpClientModule,
     StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: false }),
     EffectsModule.forRoot({}),
     EntityDataModule.forRoot(entityConfig),
   ],
