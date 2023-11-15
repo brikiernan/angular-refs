5175c8a0ca8fb702a2b5d98d571c1c06ef1029c0
Brian <brian@briankiernan.com>
2023-11-14 23:37:59 -0500

    add default data service config

--- cat-facts/src/app/fact/services/fact-override.service.ts
+++ cat-facts/src/app/fact/services/fact-override.service.ts
@@ -2,15 +2,23 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { DefaultDataService, HttpUrlGenerator } from '@ngrx/data';
+import {
+  DefaultDataService,
+  DefaultDataServiceConfig,
+  HttpUrlGenerator,
+} from '@ngrx/data';
 
 import { Fact, FactKey } from '../fact.model';
 import { ApiPath } from '../../state/app-paths';
 
 @Injectable({ providedIn: 'root' })
 export class FactOverrideService extends DefaultDataService<Fact> {
-  constructor(http: HttpClient, httpUrlGenerator: HttpUrlGenerator) {
-    super(FactKey, http, httpUrlGenerator);
+  constructor(
+    http: HttpClient,
+    httpUrlGenerator: HttpUrlGenerator,
+    config: DefaultDataServiceConfig
+  ) {
+    super(FactKey, http, httpUrlGenerator, config);
   }
 
   override getAll(): Observable<Fact[]> {
--- cat-facts/src/app/facts/services/facts-override.service.ts
+++ cat-facts/src/app/facts/services/facts-override.service.ts
@@ -2,15 +2,23 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { DefaultDataService, HttpUrlGenerator } from '@ngrx/data';
+import {
+  DefaultDataService,
+  DefaultDataServiceConfig,
+  HttpUrlGenerator,
+} from '@ngrx/data';
 
 import { Fact, FactsKey } from '../facts.model';
 import { ApiPath } from '../../state/app-paths';
 
 @Injectable({ providedIn: 'root' })
 export class FactsOverrideService extends DefaultDataService<Fact> {
-  constructor(http: HttpClient, httpUrlGenerator: HttpUrlGenerator) {
-    super(FactsKey, http, httpUrlGenerator);
+  constructor(
+    http: HttpClient,
+    httpUrlGenerator: HttpUrlGenerator,
+    config: DefaultDataServiceConfig
+  ) {
+    super(FactsKey, http, httpUrlGenerator, config);
   }
 
   override getAll(): Observable<Fact[]> {
